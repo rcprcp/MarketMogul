2a08471e58e0bb02258539aa80d94b96ac9884c3
rcprcp <bplotts@optonline.com>
2016-04-29 22:32:50 -0400

    Home arrow at the left end of the ActionBar works - in both Edit and About activities. Fix issue in AndroidManifest.xml

--- app/src/main/java/com/cottagecoders/marketmogul/About.java
+++ app/src/main/java/com/cottagecoders/marketmogul/About.java
@@ -3,19 +3,14 @@ package com.cottagecoders.marketmogul;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.Window;
 
 public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_about);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         ActionBar a = getSupportActionBar();
         a.setDisplayHomeAsUpEnabled(true);
 
--- app/src/main/java/com/cottagecoders/marketmogul/Edit.java
+++ app/src/main/java/com/cottagecoders/marketmogul/Edit.java
@@ -3,11 +3,9 @@ package com.cottagecoders.marketmogul;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -23,10 +21,7 @@ public class Edit extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_edit);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         ActionBar a = getSupportActionBar();
         a.setDisplayHomeAsUpEnabled(true);
 
